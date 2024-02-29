314161e32ecb21e6345cade493083ca06c8e4c0e
bondan mintardjo <bondan@senta.nu>
2024-02-29 09:56:01 +0800

    animation all attack points done

--- src/@core/utils/mapUtils.js
+++ src/@core/utils/mapUtils.js
@@ -52,3 +52,111 @@ export const drawArc = (id, origin, destination, mapObj) => {
     },
   );
 }
+
+let then = performance.now();
+
+export const animateLine = (id, origin, destination, mapObj) => {
+  console.log(id)
+
+  // calculate and create the coordinates array
+  let route = {
+    type: 'LineString',
+    coordinates: [
+      origin,
+      destination,
+    ],
+  };
+  const lineD = turf.lineDistance(route, { units: 'kilometers' });
+
+  const mp = turf.midpoint(destination, origin);
+
+  const center = turf.destination(
+    mp,
+    lineD,
+    turf.bearing(destination, origin) - 90,
+  );
+
+  const lA = turf.lineArc(
+    center,
+    turf.distance(center, destination),
+    turf.bearing(center, origin),
+    turf.bearing(center, destination),
+  );
+  
+  const coordinates = lA.geometry.coordinates;
+  
+  const masterRouteData = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'geometry': {
+          'type': 'LineString',
+          'coordinates': [origin], // starting point
+        },
+      },
+    ],
+  };
+
+  let newRouteData = { ...masterRouteData };
+
+  // prepare the default source and layer
+  mapObj.addSource(id, {
+    'type': 'geojson',
+    'data': newRouteData,
+  });
+  console.log(mapObj)
+  mapObj.addLayer({
+    'id': `line-animation-${id}`,
+    'type': 'line',
+    'source': id,
+    'layout': {
+      'line-cap': 'round',
+      'line-join': 'round',
+    },
+    'paint': {
+      'line-color': '#8c57ff',
+      'line-width': 2,
+      'line-opacity': 0.8,
+    },
+  });
+
+
+  // PREPARE ANIMATION
+  const fps = 10;
+  const fpsInterval = 1000 / fps;
+
+  const steps = coordinates.length;
+  let i = 0;
+  let now = performance.now();
+  let then = performance.now();
+  let elapsed;
+
+  const doAnimate = timestamp => {
+    // control fps
+    now = performance.now();
+    
+    elapsed = now - then;
+    
+    console.log(id, now, then, elapsed);
+    if (elapsed > fpsInterval) {
+      then = now - (elapsed % fpsInterval);
+      console.log('trieeger')
+
+      // codes
+      if (i >= steps) {
+        //reset..empty the map routeData coordinate
+        i = 0;
+        newRouteData.features[0].geometry.coordinates.length = 0;
+      }
+
+      newRouteData.features[0].geometry.coordinates.push(coordinates[i]);
+      mapObj.getSource(id).setData(newRouteData);
+      
+      i++;
+    }
+    requestAnimationFrame(doAnimate);
+  }
+
+  requestAnimationFrame(doAnimate);
+}
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,12 +4,11 @@ import { useAttackMapStore } from '@/stores/attacksMap';
 import { Map, MapStyle, config } from '@maptiler/sdk';
 import '@maptiler/sdk/dist/maptiler-sdk.css';
 
-import { drawArc } from '@/@core/utils/mapUtils';
+import { animateLine } from '@/@core/utils/mapUtils';
 import { markRaw, onMounted, onUnmounted, shallowRef } from 'vue';
 
 const store = useAttackMapStore()
 
-const { locations } = storeToRefs(store)
 const { getFinalAttackList } = store;
 
 const attackList = ref(computed(()=>getFinalAttackList()))
@@ -29,70 +28,11 @@ onMounted(() => {
     zoom: initialState.zoom,
   }))
   
-  // animate line function
-  var speedFactor = 15; // number of frames per longitude degree
-  var animation; // to store and cancel the animation
-  var startTime = 0;
-  var progress = 0; // progress = timestamp - startTime
-  var resetTime = false; // indicator of whether time reset is needed for the animation
-
-  function animateLine(timestamp) {
-    // restart if it finishes a loop
-    if (progress > speedFactor * 360) {
-      startTime = timestamp;
-
-      // routes[0].features[0].geometry.coordinates = origins[0];
-    } else {
-      // append new coordinates to the lineString
-      geojson.features[0].geometry.coordinates.push([x, y]);
-
-      // then update the map
-      state.map?.getSource('line').setData(routes[0]);
-    }
-
-    // Request the next frame of the animation.
-    animation = requestAnimationFrame(animateLine);
-  }
-  
   state.map.on('load', function () {
-    // get all the attackList from getAttackList() and map to drawArc
+    // get all the attackList from getAttackList() and js.map to drawArc
     attackList.value.map((attackItem, idx) => {
-      drawArc(`arc${idx}`, attackItem.from.lngLat, attackItem.to.lngLat, state.map);
+      animateLine(`arc${idx}`, attackItem.from.lngLat, attackItem.to.lngLat, state.map);
     })
-    
-
-
-    /*
-    state.map.addSource('line', {
-      'type': 'geojson',
-      'data': routes[0],
-    });
-
-    // add the line which will be modified in the animation
-    state.map?.addLayer({
-      'id': 'line-animation',
-      'type': 'line',
-      'source': 'line',
-      'layout': {
-        'line-cap': 'round',
-        'line-join': 'round',
-      },
-      'paint': {
-        'line-color': '#8c57ff',
-        'line-width': 2,
-        'line-opacity': 0.8,
-      },
-    });
-    */
-  
-    
-    startTime = performance.now();
-
-    // animateLine();
-
-    document.addEventListener('visibilitychange', function () {
-      resetTime = true;
-    });
   })
 })
 
